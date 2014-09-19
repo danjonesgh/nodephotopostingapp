6b2d41c1feccd1415310961706088af2b5be8755
Dan Jones <danjonesgh@gmail.com>
2014-09-19 00:02:29 -0400

    changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+	res.send('test');
+  //res.render('index');
 });
 
 module.exports = router;
